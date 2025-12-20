8754ec01e747546084eb090b2c84d6812e8b0b01
k-samrat108 <waste.k.samrat@gmail.com>
2025-12-20 23:32:29 +0530

    Refactor send_alert to use CHANNEL_ID directly

--- notifier.py
+++ notifier.py
@@ -3,6 +3,5 @@ from config import BOT_TOKEN, CHANNEL_ID
 
 bot = Bot(token=BOT_TOKEN)
 
-async def send_alert(text, user_id):
-    await bot.send_message(chat_id=user_id, text=text)
+async def send_alert(text):
     await bot.send_message(chat_id=CHANNEL_ID, text=text)
